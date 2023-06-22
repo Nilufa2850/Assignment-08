571c861cf4e18216637e1adea04502564160309b
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-22 20:53:49 +0530

    Create Question-12.C

---
+++ Question-12.C
@@ -0,0 +1,42 @@
+/*
+
+ABCDCBA
+ ABCBA 
+  ABA  
+   A  
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i , j ;
+    char k ;
+
+    for(i=1 ; i<=4; i++)
+    {
+            k='A';
+
+            for (j=1  ; j<=7 ; j++)
+            {
+                    if( j>=i && j<=(8-i) )
+                    {
+                            printf("%c",k);
+
+                            if(j<4)
+                                k++;
+                            else
+                                k--;   
+                    }
+
+                    else 
+                            printf(" "); 
+    
+            }
+            
+            printf("\n");
+    }
+
+    return 0;
+
+}
