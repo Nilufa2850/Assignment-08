85b3152a482e483a93c541fdf8c0b9b6c5d958ca
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-22 20:41:07 +0530

    Create Question-10.C

---
+++ Question-10.C
@@ -0,0 +1,45 @@
+/*
+
+1234321
+123 321
+12   21
+1     1
+
+*/
+
+#include<stdio.h>
+int main()
+{
+        int i , j , k;
+
+        for(i=1 ; i<=4 ; i++)
+        {
+            k=1;
+
+                for (j=1  ; j<=7 ; j++)
+                {
+                            if( j<=(5-i) || j>=(3+i) )
+                            {
+                                      printf("%d",k);
+
+                                      if(j<4)
+                                          k++;
+                                      else
+                                          k--;   
+                            }
+
+                            else 
+                            {
+                                      printf(" ");
+
+                                      if(j==4)
+                                          k-- ;
+                            }
+
+                }
+
+                printf("\n");
+        }
+        return 0;
+
+}
