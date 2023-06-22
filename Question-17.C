751977b762f03ffe9fd6e70b2e64cc692b1c3e58
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-22 22:50:12 +0530

    Create Question-17.C

---
+++ Question-17.C
@@ -0,0 +1,33 @@
+/*
+
+*********
+ *     * 
+  *   *  
+   * *   
+    *    
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i , j ;
+
+    for(i=1 ; i<=5; i++)
+    {
+            for (j=1  ; j<=9 ; j++)
+            {
+                    if( i==1 || j==i || j==(10-i))
+
+                            printf("*");
+                    else
+                            printf(" ");
+    
+            }
+            
+            printf("\n");
+    }
+
+    return 0;
+
+}
