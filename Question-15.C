2ba68368fe32c08032a07deb07584234ac5c1f0e
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-22 22:29:48 +0530

    Create Question-15.C

---
+++ Question-15.C
@@ -0,0 +1,33 @@
+/*
+
+    *
+   **
+  * *
+ *  *
+*****
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i , j ;
+
+    for(i=1 ; i<=5; i++)
+    {
+            for (j=1  ; j<=5 ; j++)
+            {
+                    if( i==5 || j==5 || j==(6-i))
+
+                            printf("*");
+                    else
+                            printf(" ");
+    
+            }
+            
+            printf("\n");
+    }
+
+    return 0;
+
+}
